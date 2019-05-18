d98f2bc97239acd1b2e2f7d42b63bbb974d84797
yibogele <596510752@qq.com>
2019-05-18 16:46:02 +0800

    add

--- effective_go/data.go
+++ effective_go/data.go
@@ -1,9 +1,53 @@
 package main
 
+import (
+	"fmt"
+)
+
+type Person struct {
+	name string
+	age int
+	home string
+
+}
+
+func NewPerson(name string, age int, home string) *Person {
+	p := new(Person)
+	p.name = name
+	p.age = age
+	p.home = home
+	return p
+}
+
+func NewPersonV2(name string, age int, home string) *Person {
+
+	p := Person{name, age, home}
+	return &p
+
+}
+
+func NewPersonV3(name string, age int, home string) *Person {
+	return &Person{name, age, home}
+}
+
+const (
+	Enone = iota
+	Eio
+	Einval
+)
 /**
  * created: 2019/5/18 13:33
  * By Will Fan
  */
 func main() {
-	$END$
+	a := [...]string   {Enone: "no error", Eio: "Eio", Einval: "invalid argument"}
+	s := []string      {Enone: "no error", Eio: "Eio", Einval: "invalid argument"}
+	m := map[int]string{Enone: "no error", Eio: "Eio", Einval: "invalid argument"}
+
+	fmt.Println(a)
+	fmt.Println(s)
+	fmt.Println(m)
+
+	fmt.Println(NewPersonV3("",1,""))
+	fmt.Println(NewPersonV3("", 1, ""))
 }
--- effective_go/interface.go
+++ effective_go/interface.go
@@ -1,9 +1,30 @@
 package main
 
+type IntPtr *int
+
+type Intf interface {
+
+}
+
+type Int int
+
+func (ip *Int) doit() {
+
+}
+
+// receiver cant be a pointer
+//func (ip IntPtr) do() {
+//
+//}
+// receiver cant be interface
+//func (itf Intf) do() {
+//
+//}
+
 /**
  * created: 2019/5/18 14:42
  * By Will Fan
  */
 func main() {
-	$END$
+	
 }
