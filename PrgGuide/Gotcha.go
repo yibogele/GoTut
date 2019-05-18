484ed9a21983a2a9002620bb462fc37e7c78208f
yibogele <596510752@qq.com>
2019-05-18 16:46:38 +0800

    add

--- PrgGuide/Gotcha.go
+++ PrgGuide/Gotcha.go
@@ -1,9 +1,14 @@
 package main
 
+import "fmt"
+
 /**
  * created: 2019/5/16 9:38
  * By Will Fan
  */
 func main() {
-	$END$
+	//var m map[string]float64
+	m := make(map[string]float64, 100)
+	m["pi"] = 3.14
+	fmt.Println(m["pi"])
 }
